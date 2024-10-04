39b5b2e43c8a72dfbd140d84f5227a00eded3792
TenshiTheCoder <tenshibusinessbrand@gmail.com>
2024-10-04 16:25:30 -0400

    saved missed changes to created components

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,10 @@
+const LoginPage = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Login Here</h1>
+    </div>
+  )
+}
+
+export default LoginPage;
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -6,3 +6,5 @@ const RegisterPage = () => {
     </div>
   )
 }
+
+export default RegisterPage
